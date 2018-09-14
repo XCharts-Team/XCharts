c45e81d2e9114cfb27b8e246b9d08a5884450c04
monitor1394 <monitor1394@gmail.com>
2018-09-15 06:52:42 +0800

    重命名为xcharts

---
+++ Demo.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using xcharts;
+
+public class Demo : MonoBehaviour
+{
+    private LineChart lineChart;
+    private float time;
+
+    void Awake () {
+        lineChart = transform.Find("line_chart").GetComponent<LineChart>();
+    }
+
+	void Update () {
+        time += Time.deltaTime;
+        if (time >= 1)
+        {
+            time = 0;
+            lineChart.AddPoint("fps", Random.Range(24.0f, 60.0f));
+            lineChart.AddPoint("rtt", Random.Range(15, 30));
+            lineChart.AddPoint("ping", Random.Range(0, 100));
+        }
+    }
+}

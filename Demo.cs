dd8a1084ab84dc22c344af4967c74b63819fcccc
monitor1394 <monitor1394@gmail.com>
2018-09-21 22:30:48 +0800

    增加动态添加数据接口AddData

--- Demo.cs
+++ Demo.cs
@@ -5,19 +5,23 @@ public class Demo : MonoBehaviour
 {
     private LineChart lineChart;
     private float time;
+    private int count;
 
-    void Awake () {
+    void Awake()
+    {
         lineChart = transform.Find("line_chart").GetComponent<LineChart>();
     }
 
-	void Update () {
+    void Update()
+    {
         time += Time.deltaTime;
         if (time >= 1)
         {
             time = 0;
-            //lineChart.AddPoint("fps", Random.Range(24.0f, 60.0f));
-            //lineChart.AddPoint("rtt", Random.Range(15, 30));
-            //lineChart.AddPoint("ping", Random.Range(0, 100));
+            count++;
+            lineChart.AddXAxisCategory("key" + count);
+            lineChart.AddData("line1", "key"+count, Random.Range(24.0f, 60.0f));
+            lineChart.AddData("line2", "key"+count, Random.Range(24.0f, 60.0f));
         }
     }
 }
--- Scripts/LineChart.cs
+++ Scripts/LineChart.cs
@@ -1,6 +1,4 @@
-﻿
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 namespace xcharts

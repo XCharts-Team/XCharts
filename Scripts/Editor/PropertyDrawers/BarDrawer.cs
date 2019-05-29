474e2e53252d05f5a6ded65c0ec82ef297ddc036
monitor1394 <monitor1394@gmail.com>
2019-05-30 00:20:00 +0800

    BarChart增加InSameBar实现非堆叠同柱

--- Scripts/Editor/PropertyDrawers/BarDrawer.cs
+++ Scripts/Editor/PropertyDrawers/BarDrawer.cs
@@ -7,12 +7,14 @@ namespace XCharts
     [CustomPropertyDrawer(typeof(BarChart.Bar), true)]
     public class BarDrawer : PropertyDrawer
     {
+        SerializedProperty m_InSameBar;
         SerializedProperty m_BarWidth;
         SerializedProperty m_Space;
         bool m_BarModuleToggle = true;
 
         private void InitProperty(SerializedProperty prop)
         {
+            m_InSameBar = prop.FindPropertyRelative("m_InSameBar");
             m_BarWidth = prop.FindPropertyRelative("m_BarWidth");
             m_Space = prop.FindPropertyRelative("m_Space");
         }
@@ -29,6 +31,8 @@ namespace XCharts
             if (m_BarModuleToggle)
             {
                 ++EditorGUI.indentLevel;
+                EditorGUI.PropertyField(drawRect, m_InSameBar);
+                drawRect.y += EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
                 EditorGUI.PropertyField(drawRect, m_BarWidth);
                 drawRect.y += EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
                 EditorGUI.PropertyField(drawRect, m_Space);
@@ -40,7 +44,7 @@ namespace XCharts
         public override float GetPropertyHeight(SerializedProperty prop, GUIContent label)
         {
             if (m_BarModuleToggle)
-                return 3 * EditorGUIUtility.singleLineHeight + 2 * EditorGUIUtility.standardVerticalSpacing;
+                return 4 * EditorGUIUtility.singleLineHeight + 3 * EditorGUIUtility.standardVerticalSpacing;
             else
                 return 1 * EditorGUIUtility.singleLineHeight + 1 * EditorGUIUtility.standardVerticalSpacing;
         }

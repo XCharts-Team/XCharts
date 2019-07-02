e6df4ea73a34d4bf05e21525f36b707de3d6fe51
monitor1394 <monitor1394@gmail.com>
2019-07-02 18:33:12 +0800

    增加selected等相关参数配置PieChart的选中效果

--- Scripts/UI/Internal/Pie.cs
+++ Scripts/UI/Internal/Pie.cs
@@ -9,6 +9,9 @@ namespace XCharts
         [SerializeField] private float m_InsideRadius;
         [SerializeField] private float m_OutsideRadius;
         [SerializeField] private float m_TooltipExtraRadius;
+        [SerializeField] private bool m_Selected;
+        [SerializeField] private int m_SelectedIndex;
+        [SerializeField] private float m_SelectedOffset;
         [SerializeField] private bool m_Rose;
         [SerializeField] private float m_Space;
         [SerializeField] private float m_Left;
@@ -20,6 +23,9 @@ namespace XCharts
         public float insideRadius { get { return m_InsideRadius; } set { m_InsideRadius = value; } }
         public float outsideRadius { get { return m_OutsideRadius; } set { m_OutsideRadius = value; } }
         public float tooltipExtraRadius { get { return m_TooltipExtraRadius; } set { m_TooltipExtraRadius = value; } }
+        public bool selected { get { return m_Selected; } set { m_Selected = value; } }
+        public int selectedIndex { get { return m_SelectedIndex; } set { m_SelectedIndex = value; } }
+        public float selectedOffset { get { return m_SelectedOffset; } set { m_SelectedOffset = value; } }
         public bool rose { get { return m_Rose; } set { m_Rose = value; } }
         public float space { get { return m_Space; } set { m_Space = value; } }
         public float left { get { return m_Left; } set { m_Left = value; } }
@@ -37,7 +43,9 @@ namespace XCharts
                     m_InsideRadius = 0f,
                     m_OutsideRadius = 80f,
                     m_TooltipExtraRadius = 10f,
-                    m_Rose = false
+                    m_Rose = false,
+                    m_Selected = false,
+                    m_SelectedOffset = 10,
                 };
                 return pie;
             }

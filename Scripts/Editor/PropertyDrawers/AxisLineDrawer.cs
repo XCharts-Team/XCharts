cd328d50b17f9db3cd5a6771c68a0e3e12427795
monitor1394 <monitor1394@gmail.com>
2019-08-22 19:16:14 +0800

    增加AxisLine的Width配置支持

--- Scripts/Editor/PropertyDrawers/AxisLineDrawer.cs
+++ Scripts/Editor/PropertyDrawers/AxisLineDrawer.cs
@@ -15,6 +15,7 @@ namespace XCharts
             drawRect.height = EditorGUIUtility.singleLineHeight;
             SerializedProperty show = prop.FindPropertyRelative("m_Show");
             SerializedProperty m_OnZero = prop.FindPropertyRelative("m_OnZero");
+            SerializedProperty m_Width = prop.FindPropertyRelative("m_Width");
             SerializedProperty m_Symbol = prop.FindPropertyRelative("m_Symbol");
             SerializedProperty m_SymbolWidth = prop.FindPropertyRelative("m_SymbolWidth");
             SerializedProperty m_SymbolHeight = prop.FindPropertyRelative("m_SymbolHeight");
@@ -27,6 +28,8 @@ namespace XCharts
             {
                 ++EditorGUI.indentLevel;
                 EditorGUI.PropertyField(drawRect, m_OnZero);
+                drawRect.y += EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
+                EditorGUI.PropertyField(drawRect, m_Width);
                 drawRect.y += EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
                 EditorGUI.PropertyField(drawRect, m_Symbol);
                 drawRect.y += EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
@@ -47,7 +50,7 @@ namespace XCharts
             float height = 0;
             if (ChartEditorHelper.IsToggle(m_AxisLineToggle,prop))
             {
-                height += 6 * EditorGUIUtility.singleLineHeight + 7 * EditorGUIUtility.standardVerticalSpacing;
+                height += 7 * EditorGUIUtility.singleLineHeight + 6 * EditorGUIUtility.standardVerticalSpacing;
             }
             return height;
         }

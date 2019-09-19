34a64f9c37188dd22d515bd60f18107ef82fa090
monitor1394 <monitor1394@gmail.com>
2019-09-19 09:06:04 +0800

    增加Tooltip的FixedWidth、FixedHeight、MinWidth、MinHeight设置支持

--- Scripts/Editor/PropertyDrawers/TooltipDrawer.cs
+++ Scripts/Editor/PropertyDrawers/TooltipDrawer.cs
@@ -14,6 +14,10 @@ namespace XCharts
             drawRect.height = EditorGUIUtility.singleLineHeight;
             SerializedProperty show = prop.FindPropertyRelative("m_Show");
             SerializedProperty type = prop.FindPropertyRelative("m_Type");
+            SerializedProperty m_FixedWidth = prop.FindPropertyRelative("m_FixedWidth");
+            SerializedProperty m_FixedHeight = prop.FindPropertyRelative("m_FixedHeight");
+            SerializedProperty m_MinWidth = prop.FindPropertyRelative("m_MinWidth");
+            SerializedProperty m_MinHeight = prop.FindPropertyRelative("m_MinHeight");
 
             ChartEditorHelper.MakeFoldout(ref drawRect, ref m_TooltipModuleToggle, "Tooltip", show);
             drawRect.y += EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
@@ -21,13 +25,21 @@ namespace XCharts
             {
                 EditorGUI.PropertyField(drawRect, type);
                 drawRect.y += EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
+                EditorGUI.PropertyField(drawRect, m_FixedWidth);
+                drawRect.y += EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
+                EditorGUI.PropertyField(drawRect, m_FixedHeight);
+                drawRect.y += EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
+                EditorGUI.PropertyField(drawRect, m_MinWidth);
+                drawRect.y += EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
+                EditorGUI.PropertyField(drawRect, m_MinHeight);
+                drawRect.y += EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
             }
         }
 
         public override float GetPropertyHeight(SerializedProperty prop, GUIContent label)
         {
             if (m_TooltipModuleToggle)
-                return 2 * EditorGUIUtility.singleLineHeight + 1 * EditorGUIUtility.standardVerticalSpacing;
+                return 6 * EditorGUIUtility.singleLineHeight + 5 * EditorGUIUtility.standardVerticalSpacing;
             else
                 return EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
         }

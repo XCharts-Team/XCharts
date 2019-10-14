612c6f5ce7634769f215507c8a0a41f77e3a57c3
monitor1394 <monitor1394@gmail.com>
2019-10-14 09:46:27 +0800

    增加SerieData的radius自定义数据项的半径

---
+++ Demo/Scripts/Demo30_Pie.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using XCharts;
+
+[DisallowMultipleComponent]
+[ExecuteInEditMode]
+public class Demo30_Pie : MonoBehaviour
+{
+    private PieChart chart;
+
+    void Awake()
+    {
+        chart = gameObject.GetComponent<PieChart>();
+        if (chart == null) return;
+        
+        var serie = chart.series.GetSerie(0);
+        var serieData = serie.GetSerieData(0);
+        serieData.radius = 100;
+    }
+}

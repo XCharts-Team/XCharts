616df1c2f5409fb9aa6a47f31d6e29ba2150a0c2
monitor1394 <monitor1394@gmail.com>
2019-10-14 12:41:12 +0800

    调整代码结构

--- Assets/XCharts/Scripts/UI/Helper/VisualMapHelper.cs
+++ Assets/XCharts/Scripts/UI/Helper/VisualMapHelper.cs
@@ -1,11 +1,3 @@
-using System.Text;
-using System;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
-using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.UI;
-
 namespace XCharts
 {
     public static class VisualMapHelper

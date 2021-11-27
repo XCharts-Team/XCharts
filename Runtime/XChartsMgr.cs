da9de442919f2e9ba39a27a1ad2eb093285c9e86
monitor1394 <monitor1394@gmail.com>
2021-11-27 22:12:08 +0800

    v2.5.0

--- Runtime/XChartsMgr.cs
+++ Runtime/XChartsMgr.cs
@@ -33,8 +33,8 @@ namespace XCharts
     [ExecuteInEditMode]
     public class XChartsMgr : MonoBehaviour
     {
-        internal static string _version = "2.4.0";
-        internal static int _versionDate = 20210831;
+        internal static string _version = "2.5.0";
+        internal static int _versionDate = 20211127;
         public static string version { get { return _version; } }
         public static int versionDate { get { return _versionDate; } }
         public static string fullVersion { get { return version + "-" + versionDate; } }

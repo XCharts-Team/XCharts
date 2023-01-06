8288e75445e85176fdc403fc8aafd676f3b380a6
monitor1394 <monitor1394@gmail.com>
2023-01-06 08:35:16 +0800

    优化gameObject被Destory后的判断

--- Runtime/Internal/Painter.cs
+++ Runtime/Internal/Painter.cs
@@ -23,7 +23,7 @@ namespace XCharts.Runtime
         public Type type { get { return m_Type; } set { m_Type = value; } }
         public void Refresh()
         {
-            if (gameObject == null) return;
+            if (null == this || gameObject == null) return;
             if (!gameObject.activeSelf) return;
             m_Refresh = true;
         }

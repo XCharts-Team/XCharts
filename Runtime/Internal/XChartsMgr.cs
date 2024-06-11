2fe9a40f4257a25453dc0347008dd55eaafe1820
monitor1394 <monitor1394@gmail.com>
2024-06-11 08:28:46 +0800

    3.11.0-preview2

--- Runtime/Internal/XChartsMgr.cs
+++ Runtime/Internal/XChartsMgr.cs
@@ -21,8 +21,8 @@ namespace XCharts.Runtime
     [ExecuteInEditMode]
     public static class XChartsMgr
     {
-        public static readonly string version = "3.11.0-preview1";
-        public static readonly int versionDate = 20240520;
+        public static readonly string version = "3.11.0-preview2";
+        public static readonly int versionDate = 20240611;
         public static string fullVersion { get { return version + "-" + versionDate; } }
 
         internal static List<BaseChart> chartList = new List<BaseChart>();
